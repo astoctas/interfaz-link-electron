115480263c2c0d7a30593d2c5a4531368e60da6d
astoctas <alejandro.lavagnino@gmail.com>
2022-08-07 19:40:56 -0300

    UI

--- src/preload.js
+++ src/preload.js
@@ -3,3 +3,7 @@ const { contextBridge, ipcRenderer } = require('electron')
 contextBridge.exposeInMainWorld('serialportsAPI',{
   listSerialPorts: () => ipcRenderer.invoke('serialport:list')
 })
+
+contextBridge.exposeInMainWorld('socketAPI',{
+  listSockets: () => ipcRenderer.invoke('socket:list')
+})
--- src/renderer.js
+++ src/renderer.js
@@ -1,6 +1,18 @@
 
 
+var $sel = document.getElementById('select-ports');
+$sel.innerHTML = "";
+
 const b = window.serialportsAPI.listSerialPorts()
 b.then(c => console.log(c))
-console.log(b);
 
+var socketPort = 4268;
+
+const _ips = window.socketAPI.listSockets();
+_ips.then(ips => {
+    ips.forEach(function(i,v){
+        document.getElementById("socket-msg").innerHTML = (document.getElementById("socket-msg").innerHTML +i+":"+socketPort+ "<br/>");
+      })
+})
+
+M.AutoInit();
